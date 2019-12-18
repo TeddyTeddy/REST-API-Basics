9c719710f104bca8981fb7f535963ed7ebce9c9e
HC <hakan.cuzdan@yandex.com>
2019-12-18 17:12:47 +0200

    Each blogpost will show its title and the user

--- postings/models.py
+++ postings/models.py
@@ -14,7 +14,7 @@ class BlogPost(models.Model):
     timestamp   = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.user.username)
+        return f'{self.title} - {str(self.user.username)}'
 
     @property
     def owner(self):
