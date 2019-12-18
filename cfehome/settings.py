2b6a01d89a328a0750e71c63e9f2ef59fa05c626
HC <hakan.cuzdan@yandex.com>
2019-12-18 15:49:34 +0200

    settings.py : django_heroku is used

--- cfehome/settings.py
+++ cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -148,3 +149,5 @@ REST_FRAMEWORK = {
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
